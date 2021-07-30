2df75e4ada2f7eace206103e6341f19354803024
Arpit Goyal <50814175+Arpitgoyalgg@users.noreply.github.com>
2021-07-30 18:56:23 +0530

    Split Chat into multiple separate components

---
+++ src/components/SingleContact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contact">
+    <div class="pic rogers">
+    </div>
+    <div class="badge">14</div>
+    <div class="name">Steve Rogers</div>
+    <div class="message">That is America's ass 🇺🇸🍑</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleContact"
+}
+</script>
+
+<style scoped>
+
+</style>
